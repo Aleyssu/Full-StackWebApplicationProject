b65e96e98a4575e6b02e81a4060815c765fcfa04
DailySugar <alexsunn30@gmail.com>
2024-10-21 19:36:37 -0400

    -Added the ability to complete orders -Edited some variables for consistency -Added order ID

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CISC327-Assignment-1
+
+## To run
+Make sure you have the required python packages installed. To do this, use
+~~~bash
+pip install -r requirements.txt
+~~~
+Once the requirements are installed, simply run `app.py` and go to the link shown in the console.
--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect
+from uuid import uuid4
+import sys
 
 app = Flask(__name__)
 
 # Fake order data
 orders = [
-    {"name": "Charlie", "drug": "H2O", "qty": "1", "date": "09/30/24"},
-    {"name": "Alice", "drug": "Dihydrogen monoxide", "qty": "10000", "date": "09/19/24"},
-    {"name": "Bob", "drug": "Hydrogen hydroxide", "qty": "42", "date": "09/24/24"},
-    {"name": "Charlie", "drug": "H2O", "qty": "1", "date": "09/30/24"},
-    {"name": "Charlie", "drug": "H2O", "qty": "1", "date": "09/30/24"},
-    {"name": "Charlie", "drug": "H2O", "qty": "1", "date": "09/30/24"},
-    {"name": "Charlie", "drug": "H2O", "qty": "1", "date": "09/30/24"}
+    {"name": "Charlie", "drug": "H2O", "qty": 1, "date": "09/30/24", "id":uuid4().hex},
+    {"name": "Alice", "drug": "Dihydrogen Monoxide", "qty": 10000, "date": "09/19/24", "id":uuid4().hex},
+    {"name": "Bob", "drug": "Hydrogen Hydroxide", "qty": 42, "date": "09/24/24", "id":uuid4().hex}
 ]
 
 # Fake inventory
 drug_inventory = [
-    {"name": "H2O", "quantity": 1000000, "expires" : "N/A"},
-    {"name": "Hydrogen Hydroxide", "quantity": 200, "expires" : "N/A"},
-    {"name": "Dihydrogen Monoxide", "quantity": 800000, "expires" : "N/A"}
+    {"name": "H2O", "qty": 1000000, "expires" : "N/A"},
+    {"name": "Hydrogen Hydroxide", "qty": 200, "expires" : "N/A"},
+    {"name": "Dihydrogen Monoxide", "qty": 800000, "expires" : "N/A"}
 ]
 
 @app.route('/index', methods=['GET', 'POST'])
@@ -31,6 +29,7 @@ def index():
 @app.route('/')
 def get_orders():
     # Default to sorting by date
+    print(orders, flush=True)
     sort_key = request.args.get('sort', 'date')
     sorted_orders = sorted(orders, key=lambda x: x[sort_key])
     return render_template('orders.html', orders=sorted_orders, sort_key=sort_key)
@@ -45,9 +44,28 @@ def create_order():
     # Do something with the inputs (e.g., save them to a database, process data, etc.)
     dateNow = datetime.now()
     date = str(dateNow.month) + "/" + str(dateNow.day) + "/" + str(dateNow.year)
-    orders.append({"name": name, "drug": drug, "qty": qty, "date": date})
+    orders.append({"name": name, "drug": drug, "qty": qty, "date": date, "id": uuid4().hex})
     
     # Redirect back to the home page or a success page
+    return redirect('/')
+    
+@app.route('/complete_order/<order_id>', methods=["POST"])
+def complete_order(order_id):
+    
+    print("Order id: ", order_id, flush=True)
+    
+    for o in orders:
+        print(o["id"], flush=True)
+    order = [o for o in orders if o['id'] == order_id][0]
+    qty_removed = int(order["qty"])
+    drug_name = order["drug"]
+    
+    drug_entry = [drug for drug in drug_inventory if drug["name"] == drug_name][0]
+    drug_entry["qty"] -= qty_removed
+    
+    orders.remove(order)
+    
+    # Redirect back to the home page or a success page
     return redirect('/')
     
 @app.route('/inventory')
