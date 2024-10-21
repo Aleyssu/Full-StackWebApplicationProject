3f9a7c2f89aec4862e31c962ee9a67e643093036
Ewan Peterson <ewanpeterson77@gmail.com>
2024-10-21 12:57:55 -0400

    inventory page

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\alexs\Actual Documents\Code\CISC327\CISC327-Assignment-1\venv
+set VIRTUAL_ENV=C:\Users\Ewan\Documents\Repos\CISC327-Assignment-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
