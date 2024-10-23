f3b1b2cc5e78d4ab3753b0d95118428b0748d883
DailySugar <alexsunn30@gmail.com>
2024-10-22 20:30:43 -0400

    -Added testing scripts for my flask frontend implementations -Removed version lock from requirements.txt as it seems to no longer be necessary

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from uuid import uuid4
 import sys
 
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 # Fake order data
 orders = [
-    {"name": "Charlie", "drug": "H2O", "qty": 1, "date": "09/30/24", "id":uuid4().hex},
-    {"name": "Alice", "drug": "Dihydrogen Monoxide", "qty": 10000, "date": "09/19/24", "id":uuid4().hex},
-    {"name": "Bob", "drug": "Hydrogen Hydroxide", "qty": 42, "date": "09/24/24", "id":uuid4().hex}
+    {"name": "Charlie", "drug": "H2O", "qty": 1, "date": "09/30/24", "id":str(uuid4())},
+    {"name": "Alice", "drug": "Dihydrogen Monoxide", "qty": 10000, "date": "09/19/24", "id":str(uuid4())},
+    {"name": "Bob", "drug": "Hydrogen Hydroxide", "qty": 42, "date": "09/24/24", "id":str(uuid4())}
 ]
 
 # Fake inventory
@@ -18,6 +18,22 @@ drug_inventory = [
     {"name": "Hydrogen Hydroxide", "qty": 200, "expires" : "N/A"},
     {"name": "Dihydrogen Monoxide", "qty": 800000, "expires" : "N/A"}
 ]
+
+# Returns the dictionary of a drug's information from the inventory given a name
+def get_drug(name):
+    drug = [d for d in drug_inventory if d['name'] == name]
+    if len(drug) < 1:
+        return None
+    else:
+        return drug[0]
+       
+# Returns an order given its id
+def get_order(id):
+    order = [o for o in orders if o['id'] == id]
+    if len(order) < 1:
+        return None
+    else:
+        return order[0]
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -29,38 +45,30 @@ def index():
 @app.route('/')
 def get_orders():
     # Default to sorting by date
-    print(orders, flush=True)
     sort_key = request.args.get('sort', 'date')
     sorted_orders = sorted(orders, key=lambda x: x[sort_key])
+    
     return render_template('orders.html', orders=sorted_orders, sort_key=sort_key)
 
-@app.route('/create_order', methods=["POST"])
+@app.route('/create_order', methods=["GET", "POST"])
 def create_order():
-    #Create Order popup
     name = request.form.get('name')
     drug = request.form.get('drug')
-    qty = request.form.get('qty')
-
-    # Do something with the inputs (e.g., save them to a database, process data, etc.)
+    qty = int(request.form.get('qty'))
+    
     dateNow = datetime.now()
     date = str(dateNow.month) + "/" + str(dateNow.day) + "/" + str(dateNow.year)
-    orders.append({"name": name, "drug": drug, "qty": qty, "date": date, "id": uuid4().hex})
+    orders.append({"name": name, "drug": drug, "qty": qty, "date": date, "id": str(uuid4())})
     
-    # Redirect back to the home page or a success page
     return redirect('/')
     
 @app.route('/complete_order/<order_id>', methods=["POST"])
 def complete_order(order_id):
-    
-    print("Order id: ", order_id, flush=True)
-    
-    for o in orders:
-        print(o["id"], flush=True)
-    order = [o for o in orders if o['id'] == order_id][0]
-    qty_removed = int(order["qty"])
+    order = get_order(order_id)
+    qty_removed = order["qty"]
     drug_name = order["drug"]
     
-    drug_entry = [drug for drug in drug_inventory if drug["name"] == drug_name][0]
+    drug_entry = get_drug(drug_name)
     drug_entry["qty"] -= qty_removed
     
     orders.remove(order)
