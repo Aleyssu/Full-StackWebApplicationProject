5d42191c8a82d2b9eee67fb3f5480ab2f84c2358
NickAxani <21nra@queensu.ca>
2024-12-04 01:32:03 -0500

    3rd e2e, and bug fix for small window
    3rd e2e: test search for both orders and inventory
    fixed bug where if the window was small navigation bar was hidden and therefore couldn't directly clicked on anything inside

--- test_e2e.py
+++ test_e2e.py
@@ -16,6 +16,15 @@ def test_inventory_modification():
         driver.get("http://127.0.0.1:5000/")
         print("Opened app homepage.", flush=True)
 
+        # If navbar is closed open it
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
+
         # Step 2: Navigate to Inventory
         inventory_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="navbarNav"]/ul/li[2]/a'))
@@ -101,6 +110,14 @@ def test_inventory_modification():
             raise ValueError("Failed to subtract drug qty")
 
         #Step 7: Test removing drug from inventory
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
+        
         modify_inventory_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="modInvenButton"]'))
         )
@@ -151,6 +168,15 @@ def test_order_modification():
         # Start at home page
         driver.get("http://127.0.0.1:5000/")
         print("Opened Home page.", flush=True)
+        
+        # If nav bar is closed open it
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
 
         # Navigate to Inventory
         inventory_button = WebDriverWait(driver, 10).until(
@@ -198,6 +224,14 @@ def test_order_modification():
             raise ValueError("Failed to add drug")
 
         # Navigate back to Orders page
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
+        
         inventory_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a'))
         )
@@ -290,7 +324,106 @@ def test_order_modification():
         print("Order modification testing complete\n", flush=True)
         driver.quit()
 
+def test_search():
+    print("Testing search:")
+    # Set up the WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        # Step 1: Navigate to App Home page
+        driver.get("http://127.0.0.1:5000/")
+        print("Opened app homepage.", flush=True)
+        
+        # Check if navbar is open, if not open it
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
+
+        #Step 2: Navigate to Order
+        inventory_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="navbarNav"]/ul/li[1]/a'))
+        )
+        inventory_button.click()
+        print("Found orders.", flush=True)
+        
+
+        #Step 3: Input search
+        search_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'search_query'))
+        )
+        search_field.click()
+        search_field.send_keys("Charlie")
+        print("Enter search name", flush=True)
+
+        search_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'search_button'))
+        )
+        search_button.click()
+        print("Click search", flush=True)
+
+        #Step 4: Confirm and check our test worked
+        topSearch = driver.find_element(By.XPATH, '//*[@id="orders"]/div[1]/div/div/h5')
+        name = topSearch.text
+        print("Check name of Top Search", flush=True)
+        
+        if name == "Charlie":
+            print("Order search successful", flush=True)
+        else:
+            raise ValueError("Order sarch failed")
+
+        #Step 5: repeat for inventory
+
+        nav_bar = driver.find_element(By.ID, "navbarNav")
+        if nav_bar.is_displayed() == False:
+            toggle_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/nav/div/button'))
+            )
+            toggle_button.click()
+            print("Clicked navigation toggle button.")
+
+        #Navigate to Inventory
+        inventory_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="navbarNav"]/ul/li[2]/a'))
+        )
+        inventory_button.click()
+        print("Found inventory.", flush=True)
+
+        #Input search
+        search_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'search_query'))
+        )
+        search_field.click()
+        search_field.send_keys("H2O")
+        print("Enter search name", flush=True)
+        
+
+        search_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'search_button'))
+        )
+        search_button.click()
+        print("Click search", flush=True)
+
+        #Confirm and check our test worked
+        topSearch = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div[1]/div/div/h5')
+        name = topSearch.text
+        print("Check name of Top Search", flush=True)
+        
+        if name == "H2O":
+            print("Inventory search successful", flush=True)
+        else:
+            raise ValueError("Inventory search failed")
+
+    finally:
+        # Close the browser
+        print("Search Testing Complete\n", flush=True)
+        driver.quit()
+
 if __name__ == "__main__":
-    # test_inventory_modification()
+    test_inventory_modification()
     test_order_modification()
+    test_search()
 
